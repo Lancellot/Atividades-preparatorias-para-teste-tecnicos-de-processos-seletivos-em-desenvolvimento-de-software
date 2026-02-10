0cea6c8fc54cbef298b698da586ce1ffc6361bef
Assis Pires Neto <42425865+Lancellot@users.noreply.github.com>
2026-02-10 18:02:18 -0300

    teste 3

---
+++ Testes Técnicos/palavraMaisFrequente.js
@@ -0,0 +1,26 @@
+const palavraMaisFrequente = (texto) => {
+    const palavras = texto.split(' ');
+    const frequencia = {};
+
+    for (const palavra of palavras) {
+        if (frequencia[palavra]) {
+            frequencia[palavra]++;
+        } else {
+            frequencia[palavra] = 1;
+        }
+    }
+    let maisFrequente = null;
+    let maxFrequencia = 0;
+    
+    for (const palavra in frequencia) {
+        if (frequencia[palavra] > maxFrequencia) {
+            maxFrequencia = frequencia[palavra];
+            maisFrequente = palavra;
+        }
+    }
+    return maisFrequente;
+}
+
+const texto = "teste teste exemplo exemplo exemplo";
+
+console.log(palavraMaisFrequente(texto));
